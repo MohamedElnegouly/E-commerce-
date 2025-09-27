7ae3f88e9b77ac5f73b935901778f757bc51e6fd
mohamed yasser <medoyasser511@gmail.com>
2025-09-27 16:58:26 +0300

    add Dio & getIt packages & create service

---
+++ lib/core/utils/Api_service.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+  ApiService(this._dio);
+  final String _baseUrl = "https://ecommerce.routemisr.com/api/v1/";
+  Future<Map<String , dynamic>> get(
+      {required String endPoint,String? token}) async {
+    Map<String, dynamic> header = {};
+    if (token != null) {
+      header.addAll({'Authorization': 'Bearer $token'});
+    }
+    var response =
+        await _dio.get('$_baseUrl$endPoint', options: Options(headers: header));
+    return response.data;
+  }
+
+  Future<dynamic> post(
+      {required String endpoint, //رايح فين
+      required String? token, //عنده صلاحيه ولا لا
+      Map<String, dynamic>? body //فين الداتا اللى هتبعتها
+      }) async {
+    Map<String, dynamic> header = {};
+    if (token != null) {
+      header.addAll({'Authorization': 'Bearer $token'});
+    }
+    var response = await _dio.post('$_baseUrl$endpoint',
+        options: Options(headers: header), data: body);
+    return response.data;
+  }
+
+  Future<dynamic> put(
+      //بتشيل كله وتحط كله تانى
+      {required String endpoint, //رايح فين
+      required String? token, //عنده صلاحيه ولا لا
+      Map<String, dynamic>? body //فين الداتا اللى هتبعتها
+      }) async {
+    Map<String, dynamic> header = {};
+    if (token != null) {
+      header.addAll({'Authorization': 'Bearer $token'});
+    }
+    var response = await _dio.put('$_baseUrl$endpoint',
+        options: Options(headers: header), data: body);
+    return response.data;
+  }
+
+  Future<dynamic> patch(
+      //بتحط الحاجه اللى انت عاوزها بس
+      {required String endpoint, //رايح فين
+      required String? token, //عنده صلاحيه ولا لا
+      Map<String, dynamic>? body //فين الداتا اللى هتبعتها
+      }) async {
+    Map<String, dynamic> header = {};
+    if (token != null) {
+      header.addAll({'Authorization': 'Bearer $token'});
+    }
+    var response = await _dio.patch('$_baseUrl$endpoint',
+        options: Options(headers: header), data: body);
+    return response.data;
+  }
+}
