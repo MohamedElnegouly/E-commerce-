da521b842fa6306151b8b265ed2895d0bc939f3a
mohamed yasser <medoyasser511@gmail.com>
2025-10-22 22:30:35 +0300

    add auth repo to service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:e_commerce/core/utils/Api_service.dart';
+import 'package:e_commerce/features/Authentication/data/repo/auth_repo.dart';
+import 'package:e_commerce/features/Authentication/data/repo/auth_repo_impl.dart';
 import 'package:e_commerce/features/category/data/repos/categories_repo.dart';
 import 'package:e_commerce/features/category/data/repos/categories_repo_imp.dart';
 import 'package:e_commerce/features/category/presentation/manager/cubit/category_screen_cubit.dart';
@@ -12,6 +14,7 @@ void setupServiceLocator() {
   getIt.registerSingleton<ApiService>(ApiService(Dio()));
   getIt.registerSingleton<HomeRepo>(HomeRepoImpl(getIt.get<ApiService>()));
   getIt.registerSingleton<CategoriesRepo>(CategoriesRepoImp( getIt.get<ApiService>()));
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(getIt.get<ApiService>()));
   getIt.registerFactory(
     () => CategoryScreenCubit(getIt.get<CategoriesRepo>()),
   );
