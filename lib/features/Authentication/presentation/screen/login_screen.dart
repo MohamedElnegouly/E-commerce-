849906fe086bb8ed7ce8d30d819a4ca04e92f5be
mohamed yasser <medoyasser511@gmail.com>
2025-10-23 15:56:09 +0300

    finish register screen

--- lib/features/Authentication/presentation/screen/login_screen.dart
+++ lib/features/Authentication/presentation/screen/login_screen.dart
@@ -2,14 +2,9 @@ import 'package:e_commerce/core/utils/app_colors.dart';
 import 'package:e_commerce/features/Authentication/presentation/screen/widgets/login_body_view.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
