58895e670f40ed433a37541552caa24c85b9d788
mohamed yasser <medoyasser511@gmail.com>
2025-10-25 17:54:26 +0300

    display name in appBar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/core/utils/app_colors.dart';
+import 'package:e_commerce/features/Authentication/data/model/auth_model.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -8,17 +10,25 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 19, vertical: 10),
-      child: Row(
-        children: [
-          Text(
-            'Hi,Mohamed..!',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: AppColors.primary,
-            ),
-          ),
-        ],
+      child: ValueListenableBuilder(
+        //listening to hive box changes
+        valueListenable: Hive.box<AuthModel>('userBox').listenable(),
+        builder: (context, Box<AuthModel> box, _) {
+          final user = box.get('user');
+          final userName = user?.user?.name ?? 'User';
+          return Row(
+            children: [
+              Text(
+                'Welcome, $userName!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primary,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
